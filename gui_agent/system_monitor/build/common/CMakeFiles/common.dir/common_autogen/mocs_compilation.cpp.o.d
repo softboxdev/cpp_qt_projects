83b1c6e567ef13082dc20bfdc606cf2a72dc2d8e
softboxdev <soft.box.development@gmail.com>
2026-04-24 15:20:42 +0500

    files

--- gui_agent/system_monitor/build/common/CMakeFiles/common.dir/common_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-common/CMakeFiles/common.dir/common_autogen/mocs_compilation.cpp.o: \
- /home/administrator/Desktop/projects/gui_agent/system_monitor/build/common/common_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
